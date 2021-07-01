c61ea763801636d75d3ad8c2efe70974916bb252
alexadonov <alexadonovan4427@gmail.com>
2021-07-01 16:30:44 +1000

    can display content from array

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class QuoteText extends React.Component {
+    constructor(props){
+        super(props);
+    };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    render() {
+        return (
+            <div className="text">
+                <h1>{this.props.text}</h1>
+            </div>
+        )
+    }
+}
+
+class Author extends React.Component {
+    constructor(props){
+        super(props);
+    };
+
+    render() {
+        return (
+            <div className="author">
+                <span>- {this.props.author}</span>
+            </div>
+        )
+    }
+}
+  
+  class QuoteBox extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            randomIndex: 0,
+            quotes: [
+                {
+                    text: 'helloWorld',
+                    author: 'no one'
+                }
+            ]
+        }
+    };
+
+    render() {
+      return (
+        <div className="quote-box">
+            <QuoteText text={this.state.quotes[this.state.randomIndex].text}/>
+            <Author author={this.state.quotes[this.state.randomIndex].author}/>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <QuoteBox />,
+    document.getElementById('root')
+  );
+  
